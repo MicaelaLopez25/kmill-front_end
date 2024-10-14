57e68eb0477bd01f6c5af294a705c70de87b92fd
Mailen Escobar <mailenescobar5d@gmail.com>
2024-10-14 17:01:04 -0300

    le puse posicionamiento relativo y absoluto a las imagenes q use

--- Kmill-project/src/components/ProductosHome/ProductosHome.jsx
+++ Kmill-project/src/components/ProductosHome/ProductosHome.jsx
@@ -1,58 +1,56 @@
 import "./ProductosHome.css";
 import { Link, Route, Routes } from "react-router-dom";
 
+const arr = [
+  {
+    fotoUrl: "./galletas.jpeg",
+    coverFotoUrl: "./galletaline.png",
+    link: "/productos",
+    title: "Cookies",
+  },
+  {
+    fotoUrl: "./alfajor.jpg",
+    coverFotoUrl: "./alfajorline.png",
+    link: "/productos",
+    title: "Alfajores",
+  },
+  {
+    fotoUrl: "./galletas.jpg",
+    coverFotoUrl: "./linemuffin2.png",
+    link: "/productos",
+    title: "Cupcakes",
+  },
+];
+
 function ProductosHome() {
   return (
     <section className="productos-home">
       <h2 className="tituloprod">Nuestros Productos</h2>
       <div className="productos-container">
-        <div className="producto">
-          <Link to={"/productos"}>
-            <a
-              href="#seccionCookies"
-              onClick={() => scrollIntoView("seccionCookies")}
-            >
-              <img
-                src="./cookieline.png"
-                alt="cookies"
-                className="producto-imagen"
-              />
-              <h3 color="black">Cookies</h3>
-            </a>
-          </Link>
-        </div>
-
-        <div className="producto">
-          <Link to={"/productos"}>
-            <a
-              href="#seccionAlfajores"
-              onClick={() => scrollIntoView("seccionAlfajores")}
-            >
-              <img
-                src="./alfajorline.png"
-                alt="alfajores"
-                className="producto-imagen"
-              />
-              <h3>Alfajores</h3>
-            </a>
-          </Link>
-        </div>
-
-        <div className="producto">
-          <Link to={"/productos"}>
-            <a
-              href="#seccionCupcake"
-              onClick={() => scrollIntoView("seccionCupcake")}
-            >
-              <img
-                src="./cupcakeline.png"
-                alt="cupcakes"
-                className="producto-imagen"
-              />
-              <h3>Cupcakes</h3>
-            </a>
-          </Link>
-        </div>
+        {arr.map((element, index) => (
+          <div className="producto">
+            <Link to={element.link}>
+              <a
+                href="#seccionCookies"
+                onClick={() => scrollIntoView("seccionCookies")}
+              >
+                <img
+                  src={element.fotoUrl}
+                  alt="cookies"
+                  className="producto-imagen"
+                />
+                <img
+                  src={element.coverFotoUrl}
+                  alt="alfajores"
+                  className="producto-cover-imagen"
+                />
+                <h3 color="black" className="producto-title">
+                  {element.title}
+                </h3>
+              </a>
+            </Link>
+          </div>
+        ))}
       </div>
     </section>
   );
