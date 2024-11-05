50a0a7bb0b8b2b99d51de3ffff11f1691eabc569
mica <mica28368@gmail.com>
2024-11-05 15:31:22 -0300

    problema con traer todo  productos de la base de datos y de paso lista de ingredientes

--- Kmill-project/src/components/grilla.jsx
+++ Kmill-project/src/components/grilla.jsx
@@ -1,22 +1,18 @@
 export function Post({
   titulo,
   link,
-  description,
-  parrafo,
-  subtitulo,
+  Descripcion,
   ingredientes,
 }) {
   return (
     <>
       <div
         className="tarjetita"
-        style={{ marginTop: "30px", marginBottom: "30px" }}
         style={{ marginTop: "30px", marginBottom: "180px" }}
       >
         <div>
-          <img className="img-fluid bordes" src={link} alt={description} />
+          <img className="img-fluid bordes" src={link} alt={Descripcion} />
           <h2 className="h3">{titulo}</h2>
-          <h4 className="h4">{subtitulo} Ingredientes:</h4>
           <ul className="custom-list">
             {ingredientes.length > 0 ? (
               ingredientes.map((ingrediente, index) => (
